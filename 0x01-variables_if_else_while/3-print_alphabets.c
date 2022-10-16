9cbba676eb97892057a23db74fcf91f633bad3fb
lawful-hit <paulmurithi518@gmail.com>
2022-10-16 06:28:36 -0700

    Sunday commit

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,13 +9,16 @@ int main(void)
 {
 	char ch;
 
+
 	for (ch = 'a'; ch <= 'z'; ch++)
+	{
 		putchar(ch);
-
+	}
 	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
 		putchar(ch);
-
-	putchar('\n);
+	}
+	putchar('\n');
 
 	return (0);
 }
