ee157184fb0fd64c9aed661c91cc8fb0ab3f849e
lawful-hit <paulmurithi518@gmail.com>
2022-10-20 04:33:35 -0700

    final fucking commit

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,7 @@
 /**
  * print_sign - prints the sign of a number
  * @n: the number of the sign to be printed
- * Return: 1 if number is gteater than zero
+ * Return: 1 if number is greater than zero
  * 0 if number is zero
  * -1 if number is less than zero
  */
