5624e7cf5c6fcb931c08da9d43a963f6a62cf5f7
lawful-hit <paulmurithi518@gmail.com>
2022-12-09 07:27:50 -0800

    Correction to task three

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -80,7 +80,7 @@ int main(int argc, char *argv[])
 		w = write(to, buffer, r);
 		if (to == -1 || w == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: cant read from file %s\n". argv[2]);
+			dprintf(STDERR_FILENO, "Error: cant write to %s\n". argv[2]);
 			free(buffer);
 			exit(99);
 		}
